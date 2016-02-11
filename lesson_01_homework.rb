06ea294c8e6bf47eec88f44536752ad981b36516
dasetienne <etienne.leue@gmail.com>
2016-02-11 16:49:21 -0500

    Submission of first homework assignment

--- lesson_01_homework.rb
+++ lesson_01_homework.rb
@@ -1,32 +1,53 @@
 # Code Reading
 
 def full_name(first_name, last_name, title)
-  # Example comment line
+  #Defines the method full_name with the arguments first_name, last_name, title
+
 
   name = nil
+  #Sets value of variable to nil prior to entering the if statement
+
+  puts "All good so far"
+  #Checks if everything still works
 
   if title && first_name && last_name
     name = title + " " + first_name + " " + last_name
+    #Defines variable name consisting of all three arguments and adds spaces if all arguments are provided      
   elsif title && last_name
     name = title + " " + last_name
+    #Defines variable name consisting of  title and last_name and adds spaces if only these two arguments are provided
   elsif first_name && last_name
     name = first_name + " " + last_name
+    #Defines variable name consisting of first_name and last_name and adds spaces if only these two arguments are provided
   elsif first_name
     name = first_name
+    #Defines variable name consisting of first_name if only first_name is provided
   else
     raise "Oh no, that doesn't look like a name"
+    #In all other cases this message will be raised 
   end
+  #Closes the if/else statement
+
+  puts "All good so far"
+  #Checks if everything still works
 
   return name
+  #If one of the conditions is fulfilled and no message is raised, the name will be returned
 end
+#Closes the full_name method
 
 # Fix Broken Code
 
-def full_name(f, l)
-  puts f + l
-end
+puts "All good so far"
+#Checks if everything still works
 
-puts full_nam("bubba", "chuck")
+def full_name(f, l)
+  puts f.capitalize + " " + l.capitalize
+end
+#Defines full_name method with two arguments, capitalizes them and prints result with space to the console
+
+full_name("bubba", "chuck")
+#Calls method with two arguments (first_name, last_name) which triggers the second elsif statement
 
 
 # Coding
@@ -34,13 +55,35 @@ puts full_nam("bubba", "chuck")
 
 ## Enter your code for your 'add' method below
 
+def add(first_number, second_number)
+  result = first_number + second_number
+  puts result
+end
 
+add(17,10)
 
 
 ## Enter the code for you 'join_strings' method below
 
+def translator(english_word,german_word)
+  translation = english_word.capitalize + " " + german_word.capitalize
+  puts translation
+end
 
-
+translator("flowerpot","blumentopf")
 
 
 ## Enter your 'old_enough_to_vote?' method below
+
+def old_enough_to_vote (year)
+  
+  if year >= 18
+    puts "Congrats, do the right thing!"
+
+  else
+    puts "Come back later, till then: read up!"
+end
+
+end
+
+old_enough_to_vote(16)
